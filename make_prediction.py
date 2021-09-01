4a3b4d9ddaf78d0d1055faf414a09e153eb11247
Jesse Wang <jesse.wang@univ.ox.ac.uk>
2021-09-01 13:45:53 +0800

    added make_prediction.py

---
+++ make_prediction.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import modules.utils as tools
+import sys
+import os
+import joblib
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
